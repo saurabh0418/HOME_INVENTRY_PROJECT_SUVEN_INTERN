7db1d327874496638d7a5e1af167609200c19eed
saurabh nayak <68698234+saurabh0418@users.noreply.github.com>
2021-10-22 16:39:22 +0530

    Add files via upload

---
+++ Home_Inventory_PRoject_SUIVen/Home-Inventory-Items-Project-master/src/homeInventory/InventoryItem.java
@@ -0,0 +1,19 @@
+package homeInventory;
+
+public class InventoryItem {
+	public String item;
+	public String location;
+	public boolean marked;
+	public String price;
+	public String date;
+	public String store;
+	public String note;
+	public String src;
+	public String id;
+
+	public String toString() {
+		return item + " " + location;
+	}
+}
+
+// %%%%% home inventory ans roject by saurabhnayak
